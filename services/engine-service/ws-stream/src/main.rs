eed875ea78b52e630045bf93bd6b5673fff0e340
iaadi4 <adityasingh40675@gmail.com>
2025-10-07 16:58:09 +0530

    feat(engine/ws): add ws implementation for user to sub/unsub to different params

--- services/engine-service/ws-stream/src/main.rs
+++ services/engine-service/ws-stream/src/main.rs
@@ -1,3 +1,142 @@
-fn main() {
-    println!("Hello, world!");
+use fred::prelude::*;
+use futures_util::StreamExt;
+use std::io::Error;
+use std::{sync::Arc, thread};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex; // need to use this instead of std::sync::Mutex because we are in an async context
+use types::WsMessage;
+use tokio_tungstenite::tungstenite::Error as WsError;
+
+pub mod types;
+pub mod user;
+pub mod ws_manager;
+
+use user::User;
+use ws_manager::WsManager;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let addr = std::env::var("WS_STREAM_URL").expect("WS_STREAM_URL must be set");
+
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await;
+    let listener = try_socket.expect("Failed to bind");
+
+    let ws_manager = Arc::new(Mutex::new(WsManager::new().await));
+
+    let ws_manager_clone = ws_manager.clone();
+    thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(process_redis_message(ws_manager_clone));
+    });
+    // thread::spawn can't work with async functions directly since it doesn't understand Futures.
+    // By creating a new tokio runtime inside the thread, we can execute the async task within this non-async context.
+
+    // Accept new connections in a loop
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_connection(stream, ws_manager.clone()));
+    }
+
+    Ok(())
+}
+
+async fn accept_connection(stream: TcpStream, ws_manager: Arc<Mutex<WsManager>>) {
+    let user_addr = stream
+        .peer_addr()
+        .expect("connected streams should have a peer address");
+
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+
+    let (write, mut read) = ws_stream.split();
+
+    // Add a user whenever someone connects
+    let user = User::new(user_addr.to_string(), write);
+
+    // Lock the manager just long enough to add the user - to reduce the time the lock is held
+    {
+        let mut manager = ws_manager.lock().await;
+        manager.add_user(user);
+    }
+
+    println!("Connection established with addr: {}", user_addr);
+
+    while let Some(msg_result) = read.next().await {
+        match msg_result {
+            Ok(msg) => {
+                if msg.is_text() {
+                    let msg_text = msg.to_text().unwrap(); // still unwrap here because you control msg type
+                    if let Ok(data) = serde_json::from_str::<WsMessage>(msg_text) {
+                        process_data(data, &user_addr.to_string(), ws_manager.clone()).await;
+                    }
+                } else if msg.is_close() {
+                    println!("Closing Connection to user with addr: {}", user_addr);
+                    let mut manager = ws_manager.lock().await;
+                    manager.remove_user(&user_addr.to_string());
+                    break;
+                }
+            }
+            Err(e) => {
+                match e {
+                    WsError::Protocol(protocol_err) => {
+                        println!(
+                            "WebSocket protocol error from {}: {:?}",
+                            user_addr, protocol_err
+                        );
+                    }
+                    WsError::ConnectionClosed | WsError::AlreadyClosed => {
+                        println!("WebSocket connection closed from {}: {:?}", user_addr, e);
+                    }
+                    _ => {
+                        println!("WebSocket error from {}: {:?}", user_addr, e);
+                    }
+                }
+                // Remove user and break the loop on error
+                let mut manager = ws_manager.lock().await;
+                manager.remove_user(&user_addr.to_string());
+                break;
+            }
+        }
+    }
+}
+
+async fn process_data(data: WsMessage, user_addr: &str, ws_manager: Arc<Mutex<WsManager>>) {
+    let mut manager = ws_manager.lock().await;
+
+    match data.method.as_str() {
+        "SUBSCRIBE" => {
+            manager.subscribe(user_addr, data).await;
+        }
+        "UNSUBSCRIBE" => {
+            manager.unsubscribe(user_addr, data).await;
+        }
+        _ => {}
+    }
+}
+
+async fn process_redis_message(ws_manager: Arc<Mutex<WsManager>>) {
+    let mut message_stream;
+
+    // Scope the lock to only the initialization of message_stream
+    {
+        let manager = ws_manager.lock().await;
+        message_stream = manager.redis_connection.subscriber.message_rx();
+    }
+
+    println!("Listening for Redis Publisher messages");
+
+    while let Ok(message) = message_stream.recv().await {
+        let publisher_message = match message.value.convert::<String>() {
+            Ok(s) => s,
+            Err(_) => {
+                println!("Unexpected Redis Publisher message value type");
+                continue;
+            }
+        };
+
+        // Lock the manager only when you need to send the message
+        let mut manager = ws_manager.lock().await;
+        manager.send_to_ws_stream(publisher_message).await;
+    }
 }
---
+++ services/engine-service/ws-stream/src/ws_manager.rs
@@ -0,0 +1,120 @@
+use futures_util::SinkExt;
+use redis::RedisManager;
+use tokio_tungstenite::tungstenite::Message;
+
+use crate::{
+    types::{WsMessage, WsResponse},
+    user::User,
+};
+use std::collections::HashMap;
+
+pub struct WsManager {
+    pub users: HashMap<String, User>,
+    pub subscriptions: HashMap<String, Vec<String>>, // user_id -> [subscription_id]
+    pub reverse_subscriptions: HashMap<String, Vec<String>>, // subscription_id -> [user_id]
+    pub redis_connection: RedisManager,
+}
+
+impl WsManager {
+    pub async fn new() -> Self {
+        Self {
+            users: HashMap::new(),
+            subscriptions: HashMap::new(),
+            reverse_subscriptions: HashMap::new(),
+            redis_connection: RedisManager::new().await.unwrap(),
+        }
+    }
+
+    pub fn add_user(&mut self, user: User) {
+        self.users.insert(user.id.clone(), user);
+    }
+
+    pub fn remove_user(&mut self, id: &str) {
+        self.users.remove(id);
+        self.subscriptions.remove(id);
+
+        for (_, subscriptions) in self.reverse_subscriptions.iter_mut() {
+            subscriptions.retain(|user_id| user_id != id);
+        }
+    }
+
+    // {"method":"SUBSCRIBE","params":["trade.BTC_USDT"],"id":1}
+    pub async fn subscribe(&mut self, user_id: &str, message: WsMessage) {
+        if message.method == "SUBSCRIBE" {
+            let (subscription_type, asset_pair) = match message.parse_subscription() {
+                Some(result) => result,
+                None => {
+                    eprintln!("Invalid subscription format: {:?}", message.params);
+                    return;
+                }
+            };
+            let subscription_id = format!("{:?}.{:?}", subscription_type, asset_pair);
+
+            if let Some(subscriptions) = self.subscriptions.get_mut(user_id) {
+                subscriptions.push(subscription_id.clone());
+            } else {
+                self.subscriptions
+                    .insert(user_id.to_string(), vec![subscription_id.clone()]);
+            }
+
+            if let Some(users) = self.reverse_subscriptions.get_mut(&subscription_id) {
+                users.push(user_id.to_string());
+            } else {
+                self.reverse_subscriptions
+                    .insert(subscription_id.clone(), vec![user_id.to_string()]);
+
+                self.redis_connection
+                    .subscribe(subscription_id.as_str())
+                    .await
+                    .expect("Failed to subscribe in redis");
+            }
+        }
+    }
+
+    // {"method":"UNSUBSCRIBE","params":["trade.BTC_USDT"],"id":1}
+    pub async fn unsubscribe(&mut self, user_id: &str, message: WsMessage) {
+        if message.method == "UNSUBSCRIBE" {
+            let (subscription_type, asset_pair) = match message.parse_subscription() {
+                Some(result) => result,
+                None => {
+                    eprintln!("Invalid unsubscription format: {:?}", message.params);
+                    return;
+                }
+            };
+            let subscription_id = format!("{:?}.{:?}", subscription_type, asset_pair);
+
+            if let Some(subscriptions) = self.subscriptions.get_mut(user_id) {
+                subscriptions.retain(|id| id != &subscription_id);
+            }
+
+            if let Some(users) = self.reverse_subscriptions.get_mut(&subscription_id) {
+                users.retain(|id| id != user_id);
+
+                if users.is_empty() {
+                    self.reverse_subscriptions.remove(&subscription_id);
+                    self.redis_connection
+                        .unsubscribe(subscription_id.as_str())
+                        .await
+                        .expect("Failed to unsubscribe in redis");
+                }
+            }
+        }
+    }
+
+    // {"data":{"E":1727866324128584,"T":1727866324088922,"U":4977146,"a":[["1.0003","0"]],"b":[],"e":"depth","s":"BTC_USDT","u":4977146},"stream":"depth.BTC_USDT"}
+    pub async fn send_to_ws_stream(&mut self, message: String) {
+        let ws_message: WsResponse = serde_json::from_str(message.as_str()).unwrap();
+
+        if let Some(users) = self.reverse_subscriptions.get(ws_message.stream.as_str()) {
+            for user_id in users {
+                if let Some(user) = self.users.get_mut(user_id) {
+                    let user_ws_stream = &mut user.ws_stream;
+                    user_ws_stream
+                        .send(Message::Text(message.clone().into()))
+                        .await
+                        .unwrap();
+                }
+            }
+        }
+    }
+}
