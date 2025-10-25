9a4af312983103f2d6450816087fdc1c923329ee
iaadi4 <adityasingh40675@gmail.com>
2025-10-25 22:21:29 +0530

    chore(indexer): remove indexer-service due to design changes

--- indexer-service/src/db.rs
+++
@@ -1,9 +0,0 @@
-use sqlx::PgPool;
-use crate::config::Config;
-use anyhow::Result;
-
-pub async fn create_db_pool(config: &Config) -> Result<PgPool> {
-    let pool = PgPool::connect(&config.db_url)
-        .await?;
-    Ok(pool)
-}
