7a8ecaabcedc96f515164684a7f3039855c1d010
iaadi4 <adityasingh40675@gmail.com>
2025-10-29 17:25:30 +0530

    chore(sweeper-service): initial setup, load up configs

---
+++ sweeper-service/src/config.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import yaml from "yaml";
+import path from "path";
+
+export interface Config {
+  master_seed: string;
+}
+
+function loadConfig() {
+  const configPath = path.join(
+    process.cwd(),
+    "src",
+    "configs",
+    "wallet.config.yml"
+  );
+
+  try {
+    const fileCotents = fs.readFileSync(configPath, "utf-8");
+    const config = yaml.parse(fileCotents);
+
+    return config as Config;
+  } catch (error) {
+    console.error("FATAL: Could not read or parse config.yaml file.");
+    console.error("Please ensure configs/configs.yaml exists and is valid.");
+    console.error("Error details:", error);
+
+    process.exit(1);
+  }
+}
+
+export const config = loadConfig();
